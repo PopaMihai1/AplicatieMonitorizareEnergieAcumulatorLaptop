5e5cfabcc28a8b21cbab5dca947f2ff00aa1a35b
PopaMihai1 <140732170+PopaMihai1@users.noreply.github.com>
2023-08-02 08:31:15 +0300

    Update Laptop.java

--- src/Laptop.java
+++ src/Laptop.java
@@ -25,20 +25,4 @@ public class Laptop {
     public boolean esteDescarcatLaptop() {
         return acumulator.esteDescarcat();
     }
-
-    public static void main(String[] args) {
-        int capacitateAcumulator = 10000; // Capacitatea acumulatorului în mAh (miliamperi-ora)
-        Laptop laptop = new Laptop(capacitateAcumulator);
-
-        int timpSimulare = 90000; // 90 de secunde (90000 milisecunde)
-        int putereUtilizare = 100; // Cantitatea de energie consumată de laptop într-un interval (100 mAh)
-
-        laptop.utilizeazaLaptop(timpSimulare, putereUtilizare);
-
-        if (laptop.esteDescarcatLaptop()) {
-            System.out.println("Laptopul s-a descărcat complet.");
-        } else {
-            System.out.println("Laptopul mai are 10% energie, trebuie conectat alimentatorul.");
-        }
-    }
 }
